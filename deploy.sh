8aae7db9aabaf996d832828b04a575fd2e243aa1
Connie Huang <conhuang01@gmail.com>
2026-07-31 10:49:46 -0700

    fix(deploy): use production environment variables in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ COMMAND_ID=$(aws ssm send-command \
     \"docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest\",
     \"docker stop tt_app || true\",
     \"docker rm tt_app || true\",
-    \"docker run -d --name tt_app --restart always -p 80:8000 -e STORAGE_TYPE=s3 -e S3_BUCKET_NAME=tt-video-editor-storage-test -e DATABASE_TYPE=dynamodb -e DYNAMODB_TABLE_NAME=tt_video_editor_matches_dev -e AWS_REGION=$AWS_REGION -e GIT_COMMIT_SHA=$GIT_SHA $ECR_REGISTRY/$ECR_REPOSITORY:latest\"
+    \"docker run -d --name tt_app --restart always -p 80:80 -e STORAGE_TYPE=s3 -e S3_BUCKET_NAME=tt-video-editor-storage -e DATABASE_TYPE=dynamodb -e DYNAMODB_TABLE_NAME=tt_video_editor_matches -e AWS_REGION=$AWS_REGION -e GIT_COMMIT_SHA=$GIT_SHA $ECR_REGISTRY/$ECR_REPOSITORY:latest\"
   ]" \
   --region "$AWS_REGION" \
   --query "Command.CommandId" --output text)
